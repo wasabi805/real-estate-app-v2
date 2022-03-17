d1c3079f692f177a175337200dfb57a2fd762524
Tim Ocampo <ocampo@Tims-MacBook-Pro.local>
2022-03-17 12:28:40 -0700

    added mockMap image.

--- pages/city/[...city-name].tsx
+++ pages/city/[...city-name].tsx
@@ -1,8 +1,10 @@
 import React, { useContext, useEffect } from 'react'
 import AppContext from 'context/appContext'
+import Image from 'next/image'
 import 'antd/dist/antd.css'
 import Listings from '@components/City/Listings'
 import PropertySearchBar from '@components/PropertySeachBar'
+import mockMap from '../../public/mockMap.jpeg'
 
 import { Row, Col } from 'antd'
 
@@ -22,7 +24,9 @@ const CityDetails = () => {
         </Col>
       </Row>
       <Row>
-        <Col flex={3}>Map</Col>
+        <Col flex={3}>
+          <Image src={mockMap}  alt='the-five-kingdoms'/>
+          </Col>
         <Col flex={2}>
           <Row>
             <h3>Santa Monica Homes For Sale</h3>
