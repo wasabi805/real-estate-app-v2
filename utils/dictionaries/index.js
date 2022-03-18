6598cf03d22aed89b7c8d8882cf58e1cb2ba0a41
Tim Ocampo <ocampo@Tims-MacBook-Pro.local>
2022-03-18 11:22:25 -0700

    created dictionary for sorting categories.

---
+++ utils/dictionaries/index.js
@@ -0,0 +1,9 @@
+export const SORT_BY_LISTING_CATEGORIES =[
+    {key: 'price_raw' , value: 'Price'},
+    {key: 'beds' , value: 'Beds'},
+    {key: 'baths' , value: 'Baths'},
+    {key: 'sqft_raw' , value: 'Square Feet'},
+    {key: 'calculate_dollar_per_sq_ft' , value: '$/Square Feet'},
+    {key: 'address_new' , value: 'Address'},
+    {key: 'calculate_location' , value: 'Location'}, //might use neighborhoods for this
+]
