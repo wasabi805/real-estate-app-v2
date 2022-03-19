3b8494a4f1cc8e80b05ab2cf120a94f3acf5898d
Tim Ocampo <ocampo@Tims-MacBook-Pro.local>
2022-03-19 12:40:53 -0700

    after adding place holder table, confirmed that styles added for CityWrapper position fixed and scroll for listing cards and tables was originally correct.

--- components/City/ListingTable.tsx
+++ components/City/ListingTable.tsx
@@ -61,8 +61,8 @@ const ListingTable = () => {
       tags: ['nice', 'developer'],
     },
     {
-      key: 'Ted DiBiase',
-      name: '',
+      key: '2',
+      name: 'Ted DiBiase',
       age: 42,
       address: 'London No. 1 Lake Park',
       tags: ['loser'],
@@ -70,6 +70,60 @@ const ListingTable = () => {
     {
       key: '3',
       name: 'Bret Heart',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+      tags: ['cool', 'teacher'],
+    },
+
+    {
+      key: '4',
+      name: 'Rody Piper',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+      tags: ['nice', 'developer'],
+    },
+    {
+      key: '5',
+      name: 'Jim Hacksaw Duggan',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+      tags: ['loser'],
+    },
+    {
+      key: '6',
+      name: 'Randy Macho Man-Bonesaw Savage',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+      tags: ['cool', 'teacher'],
+    },
+
+    {
+      key: '7',
+      name: 'Brutus Beefcake',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+      tags: ['cool', 'teacher'],
+    },
+
+    {
+      key: '8',
+      name: 'Andre the Giant',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+      tags: ['cool', 'teacher'],
+    },
+
+    {
+      key: '9',
+      name: 'Junkyard Dog',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+      tags: ['cool', 'teacher'],
+    },
+
+    {
+      key: '10',
+      name: 'Ultimate Warrior',
       age: 32,
       address: 'Sidney No. 1 Lake Park',
       tags: ['cool', 'teacher'],
--- pages/city/styles.js
+++ pages/city/styles.js
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled'
 
 export const CityWrapper = styled.div`
-  // position : fixed;
-  // margin-top: 6.4vh;
+  position: fixed;
+  margin-top: 6.4vh;
 `
