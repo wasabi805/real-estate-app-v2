983a19906417e56ad0661658189e812fc2855382
Tim Ocampo <ocampo@Tims-MacBook-Pro.local>
2022-03-19 20:32:05 -0700

    added typescript to searchActions

--- actions/searchActions.tsx
+++ actions/searchActions.tsx
@@ -1,7 +1,9 @@
 import { IGooglePlacesAddressObj } from 'interfaces/IPropertySearchBar'
-
+import { IAction } from './interface'
 export const SET_SEARCH_FIELD = 'SET_SEARCH_FIELD'
-export const setSearchField = (char: string) => {
+export const setSearchField = (
+  char: string
+): Pick<IAction, 'type' | 'payload'> => {
   return {
     type: SET_SEARCH_FIELD,
     payload: {
@@ -14,7 +16,7 @@ export const AUTO_COMPLETE_UPDATE_INPUT_AND_FETCH_LISTINGS =
   'AUTO_COMPLETE_UPDATE_INPUT_AND_FETCH_LISTINGS'
 export const autoCompleteUpdateStateAndFetchListings = (
   addressObject: IGooglePlacesAddressObj
-) => {
+): Pick<IAction, 'type' | 'payload'> => {
   return {
     type: AUTO_COMPLETE_UPDATE_INPUT_AND_FETCH_LISTINGS,
     payload: {
@@ -25,7 +27,9 @@ export const autoCompleteUpdateStateAndFetchListings = (
 
 export const UPDATE_STATE_WITH_SEARCH_RESULTS =
   'UPDATE_STATE_WITH_SEARCH_RESULTS'
-export const updateStateWithSearchResults = (data) => {
+export const updateStateWithSearchResults = (
+  data
+): Pick<IAction, 'type' | 'payload'> => {
   return {
     type: UPDATE_STATE_WITH_SEARCH_RESULTS,
     payload: {
