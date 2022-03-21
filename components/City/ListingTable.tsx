c8f0e6b72b5fe6bfa117121be087908a7dc4f11a
Tim Ocampo <ocampo@Tims-MacBook-Pro.local>
2022-03-21 10:24:15 -0700

    mapped out table for home listings.

--- components/City/ListingTable.tsx
+++ components/City/ListingTable.tsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import AppContext from 'context/appContext'
-import { Table, Tag, Space } from 'antd'
+import { Table,} from 'antd'
 import 'antd/dist/antd.css'
 import { columnNames } from './Listings/ListingTable/columnNames'
 
@@ -9,20 +9,22 @@ const ListingTable = () => {
   const { state } = appContext
   const { searchResults } = state
   console.log(searchResults, 'searchResults')
+  const {listings} = searchResults.data
 
-  const people = [
-    {
-      key: '1',
-      price: '$1,7000,000',
-      beds: 32,
-      address: 'New York No. 1 Lake Park',
-      baths: 333,
-    },
-  
-  ]
+  const homeListings = listings.map( home=>{
+    const {property_id, price, beds, address, baths} =home
+    return{
+      key: property_id,
+      address: address,
+      price: price,
+      beds: beds,
+      baths: baths,
+    }
+  })
+
   return (
     <>
-      <Table columns={columnNames} dataSource={people} />
+      <Table columns={columnNames} dataSource={homeListings} />
     </>
   )
 }
