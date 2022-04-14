7e8d0ac2b8b7e664da396598ea6ace7eec4a830d
Tim Ocampo <ocampo@Tims-MacBook-Pro.local>
2022-04-13 17:15:42 -0700

    added functionality to close filter button when another one is clicked.

--- components/City/FilterDropdownsRow/FilterDropdownsRow.tsx
+++ components/City/FilterDropdownsRow/FilterDropdownsRow.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FilterDropdownsContainer } from '@components/City/FilterDropdownsRow/styles'
 import { DropDownMenuButton } from '@components/_common/DropDownMenuButton'
 import AllFiltersButton from '@components/City/FilterDropdownsRow/FilterComponents/AllFilters/AllFiltersButton'
@@ -16,6 +16,14 @@ import {
 
 const FilterDropdownsRow = () => {
   //  ROW CONTAINING ALL FILTER BUTTONS
+  const [activePannel, setActivePannel] = useState('0')
+  const handleSetActivePannel = (key) => {
+    if (activePannel === key) {
+      return setActivePannel('0')
+    }
+    return setActivePannel(key)
+  }
+
   return (
     <FilterDropdownsContainer>
       {/* BUTTON ONE */}
@@ -29,8 +37,22 @@ const FilterDropdownsRow = () => {
       {/* <DropDownMenuButton buttonName={'Price'} component={<Price />} /> */}
 
       {/* NEW */}
-      <DropdownButton buttonName={'Price'} component={<Price />} />
-      <DropdownButton buttonName={'FooBar'} component={<div> FooBar</div>} />
+
+      <DropdownButton
+        btnKey={'1'}
+        buttonName={'Home Tyepe'}
+        activeKey={activePannel}
+        onChange={() => handleSetActivePannel('1')}
+        component={<div>Hello</div>}
+      />
+
+      <DropdownButton
+        btnKey={'2'}
+        buttonName={'Price'}
+        activeKey={activePannel}
+        onChange={() => handleSetActivePannel('2')}
+        component={<Price />}
+      />
 
       {/* BUTTON THREE */}
       {/* <DropDownMenuButton buttonName={'Home Type'} component={<HomeType />} /> */}
--- components/_common/DropdownButton.tsx
+++ components/_common/DropdownButton.tsx
@@ -4,15 +4,20 @@ import { DropdownButtonContainer } from 'components/_common/styles'
 
 const { Panel } = Collapse
 
-const DropdownButton = ({ buttonName, component }) => {
+const DropdownButton = ({
+  buttonName,
+  component,
+  onChange,
+  activeKey,
+  btnKey,
+}) => {
   return (
     <DropdownButtonContainer>
-      <Collapse onChange={() => console.log('hello')}>
-        <Panel className="MAKETHISZINDEXLARGE" header={buttonName} key="1">
+      <Collapse onChange={onChange} activeKey={activeKey}>
+        <Panel className="MAKETHISZINDEXLARGE" header={buttonName} key={btnKey}>
           <div>{component}</div>
         </Panel>
       </Collapse>
-      ,
     </DropdownButtonContainer>
   )
 }
