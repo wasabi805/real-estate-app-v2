cac355dce2ccfcc860cfcd9d0333cb692562acf1
Tim Ocampo <ocampo@Tims-MacBook-Pro.local>
2022-04-14 18:43:16 -0700

    normalized images for Listing Cards.

--- components/City/ListingsColumn/ListingCards/styles.ts
+++ components/City/ListingsColumn/ListingCards/styles.ts
@@ -8,4 +8,12 @@ export const ListingsCardWrapper = styled.div`
     flex: 1 1 18rem;
     margin: 0.5em;
   }
+
+  [class^='ant-card-cover'] {
+    img {
+      width: 100%;
+      height: 13vw;
+      object-fit: cover;
+    }
+  }
 `
