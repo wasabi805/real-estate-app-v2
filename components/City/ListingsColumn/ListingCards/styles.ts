7b6d880d38287837dca102f45be277832717a306
Tim Ocampo <ocampo@Tims-MacBook-Pro.local>
2022-04-14 18:53:05 -0700

    added rounded borders to cards.

--- components/City/ListingsColumn/ListingCards/styles.ts
+++ components/City/ListingsColumn/ListingCards/styles.ts
@@ -9,11 +9,16 @@ export const ListingsCardWrapper = styled.div`
     margin: 0.5em;
   }
 
+  .ant-card {
+    border-radius: 0 0 6px 6px;
+  }
+
   [class^='ant-card-cover'] {
     img {
       width: 100%;
       height: 13vw;
       object-fit: cover;
+      border-radius: 6px 6px 0 0;
     }
   }
 `
