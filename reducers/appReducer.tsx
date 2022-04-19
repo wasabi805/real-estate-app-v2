c2a11f158bbbf48259997dceec57d5a5312fc8cb
Tim Ocampo <ocampo@Tims-MacBook-Pro.local>
2022-04-19 14:18:40 -0700

    will branch off to test immutablejs.

--- reducers/appReducer.tsx
+++ reducers/appReducer.tsx
@@ -53,9 +53,6 @@ const { SET_CLICKED_ROW } = ListingTableActions
 const { HOMES_VIEW_TAB_CLICKED } = ListingTabActions
 const { SET_SOLD_DATE_RANGE } = ForSaleRentSoldActions
 
-
-// NOTE : WILL MAKE A NEW BRANCH FROM HERE TO TEST IMPLEMENT IMMUTABLE.js
-
 export const initialState: IinitialState = {
   isLoginModalVisibile: false,
   user: {
